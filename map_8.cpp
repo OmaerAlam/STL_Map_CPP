24fd8181d47f299e4890128e13c88c4e985cafa9
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:27:46 +0600

    Add files via upload

---
+++ map_8.cpp
@@ -0,0 +1,24 @@
+//key are unique but value can be same.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    map<string, string> gen;
+
+    gen["omaer"] = "male";
+    gen["md"] = "male";
+    gen["alam"]= "male";
+    gen["maia"]= "female";
+
+    cout<<gen["omaer"]<<" "<<gen["alam"]<<endl;
+
+return 0;
+}
+
+
+
+
+
+
+
