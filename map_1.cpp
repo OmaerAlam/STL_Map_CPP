eb9ff70558223972c675b767048bce32a6181b34
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:25:31 +0600

    Add files via upload

---
+++ map_1.cpp
@@ -0,0 +1,18 @@
+//Mapping Declaration_1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    map<string,int>id; //string- index, int- value.
+
+    id["omaer"]=2;// index id["omaer"] value is 2.
+    id["alam"]=5;// index id["alam"] value is 5.
+    id["md"]=7;// index id["md"] value is 7.
+
+    cout<<id["alam"]<<endl;
+
+return 0;
+}
+
