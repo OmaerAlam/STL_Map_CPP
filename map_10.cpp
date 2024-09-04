9d54adbe3b13892e1938e0c735c93962f53aad86
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:28:19 +0600

    Add files via upload

---
+++ map_10.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<map<string,int>> v;
+
+    map<string, int> c;
+    c["asd"] = 1;
+
+    v.push_back(c);
+    cout<<v[0]["asd"] <<endl;
+
+return 0;
+}
+
+
+
+
+
+
+
+
