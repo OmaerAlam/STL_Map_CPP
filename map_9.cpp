14412d71f6340906ab374baadb2118b8d1ab5987
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:28:01 +0600

    Add files via upload

---
+++ map_9.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    map<string, string> gen;
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        int d;
+        cin>>s>>d;
+        id[s] = d;
+
+    }
+
+return 0;
+}
+
+
+
+
+
+
+
+
