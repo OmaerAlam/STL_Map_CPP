b374df697599a8c34794d52e1fe9a1c5673d779c
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:29:11 +0600

    Add files via upload

---
+++ map_11.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    map<int,int> c;
+    c[1] = 1;
+    c[2] = 2;
+    c[3] = 1;
+    c[3] = 2;
+
+    cout<<c.size()<<endl;
+return 0;
+}
+
+
+
+
+
+
+
+
+
